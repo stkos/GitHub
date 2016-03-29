f212ad50fcf2a00798951bb1737ed3486bd3b0fe
stkos <st.kosty@gmail.com>
2016-03-29 14:27:55 +0300

    HomeWork_03

---
+++ SimpleTest.java
@@ -0,0 +1,36 @@
+package HomeWork_ChMP;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Lenovo on 27.03.2016.
+ */
+public class SimpleTest {
+    @Test
+    public void navigateToRozetkaWebSiteAndSerchForIPhone(){
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.get("http://rozetka.com.ua/");
+
+        WebElement closeNotification = driver.findElement(By.cssSelector(".notificationPanelCross"));
+        closeNotification.click();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+
+        WebElement searchField = driver.findElement(By.xpath(".//*[@id='search']/form/div[1]/div[2]/input"));
+        searchField.clear();
+        searchField.sendKeys("iPhone 6s");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+        WebElement searchButton = driver.findElement(By.cssSelector(".btn-link-i"));
+        searchButton.click();
+
+
+    }
+}
